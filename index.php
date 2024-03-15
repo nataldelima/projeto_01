16c6296a9a19d2e92a05ef3f0befdd76f9c1844b
Natal Lima <nataldelima@gmail.com>
2024-03-14 20:31:09 -0400

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do website">
+    <meta name="keywords" content="proejeto, website, portfolio de teste">
+    <link rel="stylesheet" href="estilo/style.css">
+    <title>Projeto 01</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="center">
+            <div class="logo">
+                Logomarca
+            </div>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section class="banner-principal">
+
+        <div class="center">
+            <form action="">
+                <h2>Qual o seu melhor email?</h2>
+                <input type="email" name="email" id="email" required>
+                <input type="submit" value="Cadastrar" name="acao">
+            </form>
+        </div>
+    </section>
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w-50">
+                <h2>Natal Lima</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vel atque reprehenderit sed illo labore
+                    esse
+                    soluta deserunt ea voluptate assumenda, inventore, optio magnam quod ad provident accusamus eveniet
+                    magni.
+                </p>
+                <p>Quos ad laborum aspernatur accusamus saepe nisi quae ipsa asperiores porro rerum numquam nulla earum
+                    fugiat,
+                    velit laudantium labore deserunt nostrum vitae perferendis quod. Odio esse tempore architecto quidem
+                    corrupti.</p>
+                <p>Iusto animi fuga deserunt quaerat eos minima nesciunt nihil mollitia ab, enim vitae nulla modi
+                    veritatis
+                    omnis sequi? Sed earum facere, numquam nisi saepe aliquam delectus praesentium veniam vel eum?</p>
+
+            </div>
+
+            <div class="w-50">
+                <img src="img/natal.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="box-especialidade">
+                <h3>Ícone 1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat, delectus sequi blanditiis
+                    quos facere dolorum earum et ad veniam cumque molestiae error nesciunt eum fuga eius velit totam
+                    beatae?</p>
+            </div>
+            <div class="box-especialidade">
+                <h3>Ícone 2</h3>
+                <h3>HTML5</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat, delectus sequi blanditiis
+                    quos facere dolorum earum et ad veniam cumque molestiae error nesciunt eum fuga eius velit totam
+                    beatae?</p>
+            </div>
+            <div class="box-especialidade">
+                <h3>Ícone 3</h3>
+                <h3>JavaScript</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat, delectus sequi blanditiis
+                    quos facere dolorum earum et ad veniam cumque molestiae error nesciunt eum fuga eius velit totam
+                    beatae?</p>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="extras">
+        <div class="center">
+            <div class="w-50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+                        aliquam asperiores laudantium reiciendis reprehenderit iusto cupiditate magni eveniet tempore
+                        illum! Dolorum quaerat delectus consequatur saepe officia neque error laboriosam cum.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+                        aliquam asperiores laudantium reiciendis reprehenderit iusto cupiditate magni eveniet tempore
+                        illum! Dolorum quaerat delectus consequatur saepe officia neque error laboriosam cum.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus
+                        aliquam asperiores laudantium reiciendis reprehenderit iusto cupiditate magni eveniet tempore
+                        illum! Dolorum quaerat delectus consequatur saepe officia neque error laboriosam cum.</p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+
+            </div>
+            <div class="w-50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, ex. Quaerat perferendis
+                            iste ipsum quas accusantium laboriosam vero modi, neque exercitationem iusto veritatis,
+                            aliquid quis, eaque necessitatibus ullam officia! Rem.</li>
+                        <li>Natus, cumque consectetur voluptas at ipsam obcaecati explicabo, harum atque quidem
+                            perspiciatis quo? In iste nisi sapiente nulla expedita corrupti nemo fuga consequatur optio,
+                            dignissimos eaque sed veritatis quidem impedit!</li>
+                        <li>Natus suscipit, quasi harum, itaque quod rem sit velit cumque soluta iusto sunt,
+                            consequuntur nisi ipsam corrupti cum modi voluptatibus fuga doloremque unde? Blanditiis amet
+                            nesciunt sint dolores deserunt alias.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
